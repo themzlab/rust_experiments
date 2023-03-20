0ac6555c3bfd829147b4aff2f43f14887ae5c159
“Mark <mark.kannel@johnsonfit.com>
2023-03-19 21:26:11 -0500

    have spi reading in a thread

--- adc_module/main.py
+++ adc_module/main.py
@@ -3,6 +3,10 @@ import time
 from adc_module import AdcModule
 m = AdcModule("my_test")
 m.begin_reading()
+try:
+    m.begin_thread()
+except AttributeError:
+    print("---------------------------------begin_thread is not implemented")
 m.test()
 time.sleep(20)
 m.test()
--- adc_module/src/lib.rs
+++ adc_module/src/lib.rs
@@ -2,7 +2,7 @@ use pyo3::prelude::*;
 use std::thread::{self, JoinHandle};
 use std::time::Duration;
 use rppal::spi::{Bus, Mode, Segment, SlaveSelect, Spi};
-use std::error::Error;
+// use std::error::Error;
 // use pyo3::{wrap_pyfunction, };
 
 
@@ -32,28 +32,36 @@ impl AdcModule {
         AdcModule {name}
     }
 
-    // fn begin_thread(&self) -> Result<(), Box<dyn Error>> {
-    //     println!("MY beginning thread{:}", self.name);
-    //     let my_name = self.name.to_string();
-    //     let spi: Spi = Spi::new(Bus::Spi0, SlaveSelect::Ss0, 100_000, Mode::Mode0)?;
-    //     // create the Thread, also pass ownership of my_name into it
-    //     let _t: JoinHandle<Self> = thread::spawn(move || {
-    //         println!{"LATER, started new thread named {:}", my_name};
-    //         // do other initialization I guess
-    //         let mut counter: i32 = 0;
-    //         let mut read_buffer: [u8; 2] = [0u8; 2]; // the spi read will be according to the size of the buffer
-    //         loop {
-    //             spi.transfer_segments(&[
-    //                 // Segment::with_write(&[READ, 0, 0, 0]),
-    //                 Segment::with_read(&mut read_buffer),
-    //             ]);
-    //             counter = counter + 1;
-    //             println!("---->{:},a Rust thread! loop {:}", my_name, counter);
-    //             thread::sleep(Duration::from_secs(1));
-    //         }
-    //     });
-    //     Ok(())
-    // }
+    fn begin_thread(&self) {
+        println!("MY beginning thread{:}", self.name);
+        let my_name: String = self.name.to_string();
+        let spi: Spi = Spi::new(Bus::Spi0, SlaveSelect::Ss0, 100_000, Mode::Mode0).unwrap();
+
+        // create the Thread, also pass ownership of my_name into it
+        let _t: JoinHandle<Self> = thread::spawn(move || {
+            const OFFSET_ADC: i32 = 0;
+            const DIVISOR_ADC: f32 = 100.0;
+            println!{"LATER, started new thread named {:}", my_name};
+            // do other initialization I guess
+            let mut counter: i32 = 0;
+            let mut read_buffer: [u8; 2] = [0u8; 2]; // the spi read will be according to the size of the buffer
+            loop {
+                spi.transfer_segments(&[
+                    Segment::with_read(&mut read_buffer),
+                ]);
+                let _output = u16::from_be_bytes(read_buffer);
+                let mut output = _output as i32;
+                output = output-OFFSET_ADC;
+                let mut torque =output as f32;
+                torque = torque / DIVISOR_ADC;
+                //
+                counter = counter + 1;
+                println!("---->{:},a Rust thread! loop {:} value {:.3}", my_name, counter, torque);
+                thread::sleep(Duration::from_secs(1));
+            }
+        });
+        
+    }
 
     fn begin_reading(&self) {
         println!("begin_reading{:}", self.name);
@@ -65,14 +73,13 @@ impl AdcModule {
             let mut counter: i32 = 0;
             loop {
                 counter = counter + 1;
-                println!("---->{:},a Rust thread; begin_reading {:}", my_name, counter);
+                println!("---->{:},a Test thread; begin_reading {:}", my_name, counter);
                 thread::sleep(Duration::from_millis(500));
             }
         });
     }
 
     fn test(&self) {
-        // self.name = "hi mark".to_string();
         println!("{:}", self.name);
     }
 }
