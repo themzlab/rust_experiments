4a5a2f21f5cc7976d190443a6b67f1bb8326e4fa
“Mark <mark.kannel@johnsonfit.com>
2023-11-20 09:35:58 -0600

    rust spawns new thread

--- pwm_module/main.py
+++ pwm_module/main.py
@@ -1,7 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import time
+import threading
+import multiprocessing
+
 import pwm_module
 
 # Define 100 duty cycle values (as percentages, e.g., 0.5 for 50%)
 duty_cycles = [0.1, 0.2, 0.3, .4, .5, .6, .7, .9, .95]  # Populate this list with your values
 
 # Start the PWM control loop
-pwm_module.start_pwm(duty_cycles)
+def main():
+    print("Starting")
+    pwm_module.start_pwm(duty_cycles)
+    print("this prints because Rust spawns a new thread")
+
+_main = threading.Thread(target=main, daemon=True)
+
+_main.start()
+time.sleep(10)
+print("about to stop the pwm")
+pwm_module.stop_pwm()
+
+print("end")
--- pwm_module/src/lib.rs
+++ pwm_module/src/lib.rs
@@ -1,21 +1,54 @@
 use pyo3::prelude::*;
 use rppal::pwm::{Channel, Polarity, Pwm};
-use std::{thread, time::Duration};
+use std::{sync::Arc, thread, time::Duration, sync::atomic::{AtomicBool, Ordering}};
+use std::thread::JoinHandle;
+
+static mut RUNNING: Option<Arc<AtomicBool>> = None;
+static mut PWM_THREAD: Option<JoinHandle<()>> = None;
 
 #[pyfunction]
 fn start_pwm(duty_cycles: Vec<f64>) -> PyResult<()> {
-    let pwm = Pwm::with_frequency(Channel::Pwm0, 20_000.0, 0.0, Polarity::Normal, true).unwrap();
-
-    for duty_cycle in duty_cycles.iter().cycle() {
-        pwm.set_duty_cycle(*duty_cycle).unwrap();
-        thread::sleep(Duration::from_millis(10));
+    let running = Arc::new(AtomicBool::new(true));
+    unsafe {
+        RUNNING = Some(running.clone());
     }
 
+    let handle = thread::spawn(move || {
+        let pwm = Pwm::with_frequency(Channel::Pwm0, 20_000.0, 0.0, Polarity::Normal, true).unwrap();
+
+        for duty_cycle in duty_cycles.iter().cycle() {
+            if !running.load(Ordering::SeqCst) {
+                break;
+            }
+            pwm.set_duty_cycle(*duty_cycle).unwrap();
+            thread::sleep(Duration::from_millis(10));
+        }
+    });
+
+    unsafe {
+        PWM_THREAD = Some(handle);
+    }
+
+    Ok(())
+}
+
+#[pyfunction]
+fn stop_pwm() -> PyResult<()> {
+    unsafe {
+        if let Some(running) = &RUNNING {
+            running.store(false, Ordering::SeqCst);
+            if let Some(handle) = PWM_THREAD.take() {
+                let _ = handle.join();
+            }
+        }
+    }
     Ok(())
 }
 
 #[pymodule]
 fn pwm_module(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(start_pwm, m)?)?;
+    m.add_function(wrap_pyfunction!(stop_pwm, m)?)?;
     Ok(())
 }
+
