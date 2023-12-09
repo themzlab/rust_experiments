6935c53f889ad00a7011e5595061a82c4334f788
Mark Kannel <lmm305-themzlab@yahoo.com>
2023-12-09 08:49:53 -0600

    still works conc_test

--- conc_test/src/main.rs
+++ conc_test/src/main.rs
@@ -7,28 +7,20 @@ fn main() {
     // wrapped in an Arc for safe sharing across threads
     let pair = Arc::new((Mutex::new(false), Condvar::new()));
     
-    // Spawn two threads
+    // Spawn Four threads
     for i in 1..=4 {
         let pair_clone = pair.clone();
         thread::spawn(move || {
 
             // Clone the Arc to pass a reference to the shared state to the threads
-            let (lock, cvar) = &*pair_clone;
+            let (my_mutex, my_conditional_variable) = &*pair_clone;
 
-            let mut started = lock.lock().unwrap();
+            let mut started = my_mutex.lock().unwrap();
 
             loop {
-                // Clone the Arc to pass a reference to the shared state to the threads
-                // let (lock, cvar) = &*pair_clone;
 
-                // Lock the mutex to access the shared state
-                // let mut started = lock.lock().unwrap();
-
-                
-                // println!("Thread {}: Message flag {}", i, started);
-
-                started = cvar.wait(started).unwrap();
-
+                started = my_conditional_variable.wait(started).unwrap();
+            
                 // Reset the flag
                 *started = false;
 
@@ -41,21 +33,19 @@ fn main() {
 
     thread::sleep(Duration::from_millis(100));
     // Loop in the main thread to signal every second
-    let (lock, cvar) = &*pair;
-    // let mut started = lock.lock().unwrap();
-    // let mut started = lock.lock();
+    let (my_mutex, cvar) = &*pair;
 
-    for _ in 0..10 {
+    for i in 0..10 {
         thread::sleep(Duration::from_millis(2));
 
         // Signal the condition variable
         {
-            // let (lock, cvar) = &*pair;
-            let mut started = lock.lock().unwrap();
-            // started = lock.lock().unwrap();
+            let mut started = my_mutex.lock().unwrap();
+
             *started = true;
             cvar.notify_all();
-            println!("  end notify");
+            println!("  end notify {}", i);
         }
     }
+    thread::sleep(Duration::from_millis(100))
 }
