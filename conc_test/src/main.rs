4458a709e7037dd028a83ca2c6277601f07e0bb0
Mark Kannel <lmm305-themzlab@yahoo.com>
2023-12-09 09:30:54 -0600

    conc_test works and better annotated

--- conc_test/src/main.rs
+++ conc_test/src/main.rs
@@ -5,27 +5,25 @@ use std::time::Duration;
 fn main() {
     // Create a shared state (boolean flag) and a condition variable, 
     // wrapped in an Arc for safe sharing across threads
-    let pair = Arc::new((Mutex::new(false), Condvar::new()));
+    let pair: Arc<(Mutex<bool>, Condvar)> = Arc::new((Mutex::new(false), Condvar::new()));
     
-    // Spawn Four threads
-    for i in 1..=4 {
-        let pair_clone = pair.clone();
+    // Spawn a bunch of  threads
+    for i in 1..=5 {
+        let pair_clone: Arc<(Mutex<bool>, Condvar)> = pair.clone();
         thread::spawn(move || {
 
             // Clone the Arc to pass a reference to the shared state to the threads
             let (my_mutex, my_conditional_variable) = &*pair_clone;
 
-            let mut started = my_mutex.lock().unwrap();
+            let mut started: std::sync::MutexGuard<'_, bool> = my_mutex.lock().unwrap();
 
             loop {
 
                 started = my_conditional_variable.wait(started).unwrap();
-            
-                // Reset the flag
-                *started = false;
 
-                // Print a message
                 println!("Thread {}: Message flag {}", i, started);
+                // Reset the flag, but only one thread will get to to this, the value is
+                *started = false;
 
             }
         });
@@ -39,13 +37,21 @@ fn main() {
         thread::sleep(Duration::from_millis(2));
 
         // Signal the condition variable
+        // When the block is entered, the mutex is locked, and the shared state (started) is 
+        // accessed and modified
         {
-            let mut started = my_mutex.lock().unwrap();
+            let mut started: std::sync::MutexGuard<'_, bool> = my_mutex.lock().unwrap();
 
             *started = true;
+            // this seems to be a mechanism to notify the other threads that the value of started has changed
             cvar.notify_all();
-            println!("  end notify {}", i);
+            println!("\nend notify {} started= {}\n", i, started)
         }
+        // the other threads will not advance even though the notification has been sent
+        // until the code block goes out of scope and releases my_mutex
+        // the lock guard returned by lock() goes out of scope. This automatically releases
+        // the lock on the mutex
     }
+    
     thread::sleep(Duration::from_millis(100))
 }
