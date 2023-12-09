d12a5e063a02e2fd487f18c005d7732e8d40d98f
Mark Kannel <lmm305-themzlab@yahoo.com>
2023-12-09 12:30:32 -0600

    main thread with two subloops working

--- conc_test/src/main.rs
+++ conc_test/src/main.rs
@@ -8,83 +8,95 @@ fn main() {
     let pair: Arc<(Mutex<bool>, Condvar)> = Arc::new((Mutex::new(false), Condvar::new()));
     let running = Arc::new(AtomicBool::new(true));
 
-    // Spawn a bunch of  threads
-    for i in 1..=1 {
-        let pair_clone: Arc<(Mutex<bool>, Condvar)> = pair.clone();
-        let my_running = running.clone();
+    let pair_clone: Arc<(Mutex<bool>, Condvar)> = pair.clone();
+    let my_running = running.clone();
 
-        thread::spawn(move || {
+    // make just one super thread
+    thread::spawn(move || {
 
-            // Clone the Arc to pass a reference to the shared state to the threads
-            let (my_mutex, my_conditional_variable) = &*pair_clone;
-            loop {
+        // Clone the Arc to pass a reference to the shared state to the threads
+        let (my_mutex, my_conditional_variable) = &*pair_clone;
+
+        loop {
+                {
+                    let mut started: std::sync::MutexGuard<'_, bool> = my_mutex.lock().unwrap();
+
+                    let mut i = 1;
+                    let my_interval = Duration::from_millis(50);
                     {
-                        let mut started: std::sync::MutexGuard<'_, bool> = my_mutex.lock().unwrap();
-
-                        loop {
-
-                            started = my_conditional_variable.wait(started).unwrap();
-
-                            if !my_running.load(Ordering::Relaxed) {
-                                break;
-                            }
-                            println!("Thread {}: Message flag {}", i, started);
-                            // Reset the flag, but only one thread will get to to this, the value is
-                            *started = false;
-
-                        }
+                        started = my_conditional_variable.wait(started).unwrap();
+                        *started = false;
                     }
-                    my_running.store(true, Ordering::Relaxed);
-                    println!{"go to the second loop"};
-                    // second
-                    {
-                        let mut started: std::sync::MutexGuard<'_, bool> = my_mutex.lock().unwrap();
-
-                        loop {
-
-                            started = my_conditional_variable.wait(started).unwrap();
-
-                            if !my_running.load(Ordering::Relaxed) {
-                                break;
-                            }
-
-                            println!("SECOND LOOP Thread {}: Message flag {}", i, started);
-                            // Reset the flag, but only one thread will get to to this, the value is
-                            *started = false;
-
+                    
+                    loop {
+                        if !my_running.load(Ordering::Relaxed) {
+                            break;
                         }
+                        i = i + 1;
+                        println!("content 1:  {}", i);
+                        thread::sleep(my_interval);
+                    }
+                    // why doesn't the following line ever run
+                    println!("content 1: is complete");
+                }
+                my_running.store(true, Ordering::Relaxed);
+                println!{"go to the second loop"};
+                // second
+                {
+                    let mut started: std::sync::MutexGuard<'_, bool> = my_mutex.lock().unwrap();
+
+                    started = my_conditional_variable.wait(started).unwrap();
+                    let mut i = 1;
+                    let my_interval = Duration::from_millis(50);
+                    loop {
+                        if !my_running.load(Ordering::Relaxed) {
+                            break;
+                        }
+                        i = i + 1;
+                        println!("CONTENT 2:  {}, {}", i, started);
+                        thread::sleep(my_interval);
+                    }
+                    println!("CONTENT 2: is complete");
+                    // Reset the flag, but only one thread will get to to this, the value is
+                    *started = false;
+                    if !my_running.load(Ordering::Relaxed) {
+                        break;
                     }
 
                 }
-        });
-    }
+
+            }
+    });
 
     thread::sleep(Duration::from_millis(100));
     // Loop in the main thread to signal every second
     let (my_mutex, cvar) = &*pair;
 
-    for i in 0..5 {
-        thread::sleep(Duration::from_millis(2));
+    // for i in 0..5 {
+    thread::sleep(Duration::from_millis(200));
 
-        // Signal the condition variable
-        // When the block is entered, the mutex is locked, and the shared state (started) is 
-        // accessed and modified
-        {
-            let mut started: std::sync::MutexGuard<'_, bool> = my_mutex.lock().unwrap();
-            
-            // dereference operator. access the data (bool) that the MutexGuard is pointing to
-            *started = true;
+    // Signal the condition variable
+    // When the block is entered, the mutex is locked, and the shared state (started) is 
+    // accessed and modified
+    {
+        println!("the code gets here");
+        let mut started: std::sync::MutexGuard<'_, bool> = my_mutex.lock().unwrap();
+        
+        // dereference operator. access the data (bool) that the MutexGuard is pointing to
+        *started = true;
 
-            // this seems to be a mechanism to notify the other threads that the value of started has changed
-            cvar.notify_all();
-            println!("\nend notify {} started= {}\n", i, started)
-        }
-        // the other threads will not advance even though the notification has been sent
-        // until the code block goes out of scope and releases my_mutex
-        // the lock guard returned by lock() goes out of scope. This automatically releases
-        // the lock on the mutex
+        // this seems to be a mechanism to notify the other threads that the value of started has changed
+        cvar.notify_all();
+        println!("\nend notify started= {}\n", started);
+        println!("reaches here");
     }
-
+    // the other threads will not advance even though the notification has been sent
+    // until the code block goes out of scope and releases my_mutex
+    // the lock guard returned by lock() goes out of scope. This automatically releases
+    // the lock on the mutex
+    //}
+    thread::sleep(Duration::from_millis(2000));
+    println!("ENDING notifications for content 1\n\n");
     
     running.store(false, Ordering::SeqCst);
     thread::sleep(Duration::from_millis(1000));
